8fe78c54ff4ea12516d7dc14a45b8ac7660ae161
Ryan Mercado <ryan.mercado@madmobile.com>
2024-01-19 21:34:56 -0500

    pills in VerticalGameInfo

--- src/renderer/src/components/VerticalGameInfo/index.tsx
+++ src/renderer/src/components/VerticalGameInfo/index.tsx
@@ -3,6 +3,9 @@ import css from "./VerticalGameInfo.module.scss";
 import classNames from "classnames";
 import Marquee from "../Marquee";
 import MediaImage from "../MediaImage/MediaImage";
+import useGamePills from "../Pill/hooks/useGamePills";
+import { useMemo } from "react";
+import Pill from "../Pill";
 
 interface Props {
   className?: string;
@@ -10,6 +13,18 @@ interface Props {
 }
 
 const VerticalGameInfo = ({ className, game }: Props) => {
+  const pills = useGamePills(game);
+  const pillElements = useMemo(() => (
+    pills.map(pill => (
+      <Pill
+        Icon={pill.Icon}
+        key={pill.id}
+        label={pill.text}
+        className={css.pill}
+      />
+    ))
+  ), [pills])
+
   return (
     <div className={classNames(css.container, className)}>
       <div className={css.bgAndLogo}>
@@ -21,6 +36,11 @@ const VerticalGameInfo = ({ className, game }: Props) => {
       >
         {game.description}
       </Marquee>
+      {Boolean(pillElements.length) && Boolean(game.description) &&
+        <div className={css.pills}>
+          {pillElements}
+        </div>
+      }
     </div>
   );
 }
