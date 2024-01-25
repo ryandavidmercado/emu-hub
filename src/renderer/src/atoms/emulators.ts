e04779bdc57ac44d0886f9c8dc2a2cedeaef914a
Ryan Mercado <ryan.mercado@madmobile.com>
2024-01-25 13:45:10 -0500

    use common Emulator type in emulators atom

--- src/renderer/src/atoms/emulators.ts
+++ src/renderer/src/atoms/emulators.ts
@@ -1,12 +1,4 @@
+import { Emulator } from "@common/types/Emulator";
 import { arrayConfigAtoms } from "./util/arrayConfigAtom";
-
-export type Emulator = {
-  id: string
-  name: string
-} & ({
-  core: string
-} | {
-  bin: string
-})
 
 export default arrayConfigAtoms<Emulator>({ storageKey: "emulators" });
