08a986badbbe095050ba12f00e8a3740408558b1
Ryan Mercado <ryan.mercado@madmobile.com>
2024-01-25 13:47:00 -0500

    preload game logo on defaults to ensure that the file actually exists

--- src/renderer/src/components/Showcase/presets/game/index.ts
+++ src/renderer/src/components/Showcase/presets/game/index.ts
@@ -3,11 +3,13 @@ import { Game } from "@common/types";
 import { Pill, ShowcaseContent } from "../..";
 
 export const getGameShowcaseConfig = (game: Game, pills: Pill[]): ShowcaseContent => {
+  const gameLogo = window.loadMedia(game.logo || "");
+
   return {
     left: [{ type: "media", media: game.screenshot || "" }],
     right: [
-      game.logo
-        ? { type: "media", media: game.logo || "" }
+      gameLogo
+        ? { type: "media", media: gameLogo }
         : { type: "text", text: game.name ?? game.romname, className: css.text },
       { type: "pills", pills }
     ],
