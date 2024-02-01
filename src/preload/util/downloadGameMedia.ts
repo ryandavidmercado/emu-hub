654353bd70bd2f34b6693728d521c27bd3534f64
Ryan Mercado <ryan.mercado@madmobile.com>
2024-02-01 18:00:19 -0500

    autocrop downloaded media assets

--- src/preload/util/downloadGameMedia.ts
+++ src/preload/util/downloadGameMedia.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { Game } from "@common/types";
 import { ASSETS_PATH } from "./const";
 import { mkdir, writeFile } from "fs/promises";
+import Jimp from "jimp";
 
 interface GameMedia {
   mediaType: string,
@@ -20,7 +21,18 @@ const downloadGameMedia = async (game: Game, medias: GameMedia[]) => {
     const mediaPath = path.join(gameAssetsPath, `${mediaType}.${format}`)
 
     const response = await fetch(url);
-    const buffer = Buffer.from(await response.arrayBuffer());
+
+    let buffer = Buffer.from(await response.arrayBuffer());
+    try {
+      buffer = await Jimp.read(buffer)
+        .then(img =>
+          img
+            .autocrop()
+            .getBufferAsync(Jimp.AUTO as unknown as string)
+        )
+    } catch(e) {
+      console.log(`Couldn't crop asset ${mediaType} for ${game.romname} -- ${e}`)
+    }
 
     await writeFile(mediaPath, buffer);
     return {
