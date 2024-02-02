06f9047347e5a92d084fa06286886b7e0f684238
Ryan Mercado <ryan.mercado@madmobile.com>
2024-02-01 21:11:53 -0500

    swap out Jimp for Sharp

--- src/preload/util/downloadGameMedia.ts
+++ src/preload/util/downloadGameMedia.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { Game } from "@common/types";
 import { ASSETS_PATH } from "./const";
 import { mkdir, writeFile } from "fs/promises";
-import Jimp from "jimp";
+import sharp from "sharp"
 
 interface GameMedia {
   mediaType: string,
@@ -24,14 +24,9 @@ const downloadGameMedia = async (game: Game, medias: GameMedia[]) => {
 
     let buffer = Buffer.from(await response.arrayBuffer());
     try {
-      buffer = await Jimp.read(buffer)
-        .then(img =>
-          img
-            .autocrop()
-            .getBufferAsync(Jimp.AUTO as unknown as string)
-        )
+      buffer = await sharp(buffer).trim().toBuffer();
     } catch(e) {
-      console.log(`Couldn't crop asset ${mediaType} for ${game.romname} -- ${e}`)
+      console.log(`Couldn't crop asset ${mediaType} for ${game.romname} -- ${e}`);
     }
 
     await writeFile(mediaPath, buffer);
