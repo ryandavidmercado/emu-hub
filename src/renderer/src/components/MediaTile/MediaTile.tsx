de3fbe13bc6566e9c205186cc116d360bf7a3c33
ryandavidmercado <ryandavidmercado@gmail.com>
2024-02-12 10:23:48 -0500

    revert using async for MediaTiles; no notable improvement in grid scroll & more visually inconsistent

--- src/renderer/src/components/MediaTile/MediaTile.tsx
+++ src/renderer/src/components/MediaTile/MediaTile.tsx
@@ -50,10 +50,9 @@ const MediaTile = ({
         <MediaImage
           media={media.background}
           className={classNames(css.background, media.foreground && css.withForeground)}
-          async
         />
       )}
-      {media.foreground && <MediaImage media={media.foreground} className={css.foreground} async />}
+      {media.foreground && <MediaImage media={media.foreground} className={css.foreground} />}
       {media.foregroundText && !media.foreground && !media.background && (
         <div className={css.foregroundTextWrapper}>
           <div className={css.foregroundText}>{media.foregroundText}</div>
