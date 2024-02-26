b3420b6f8d052f1d9a9c651822fdd5903f3454a0
ryandavidmercado <ryandavidmercado@gmail.com>
2024-02-26 10:21:23 -0500

    bump ver

--- src/renderer/src/const/const.ts
+++ src/renderer/src/const/const.ts
@@ -1,5 +1,5 @@
 export const APPNAME = 'EmuHub'
-export const version = '1.0.0-beta.1'
+export const version = '1.0.0-beta.2'
 export const SOFTNAME = `${APPNAME} v${version}`
 
 export const displayTypeMap = {
